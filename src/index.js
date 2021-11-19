c60537b9b260c2295a5618d99198cb22f3966560
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-11-18 21:43:09 -0400

    setting up webpack

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("Configuration of webpack")
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPluging = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    output: {
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPluging()
+    ]
+}
