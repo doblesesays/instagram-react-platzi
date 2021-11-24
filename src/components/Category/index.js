cf19158710ef06723972d398957a666951d9d7b2
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-11-23 21:57:05 -0400

    Componente Category y styled components

---
+++ src/components/Category/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Anchor, Image } from './styles';
+
+const DEFAULT_IMG = 'https://i.imgur.com/dJa0Hpl.jpg'
+
+export const Category = ({ cover = DEFAULT_IMG, path, emoji = '?' }) => (
+    <Anchor href={path}>
+        <Image src={cover} />
+        { emoji }
+    </Anchor>
+)
