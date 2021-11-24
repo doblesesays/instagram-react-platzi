758478622fbbd860c7e70d384c90198fe819667e
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-11-23 21:32:47 -0400

    Deploy on Vercel (former Now)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Configuration of webpack")
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(<h1>Deploy on Vercel (former Now)</h1>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
-const HtmlWebpackPluging = require('html-webpack-plugin');
+const HtmlWebpackPluging = require('html-webpack-plugin')
 module.exports = {
-    mode: 'development',
-    output: {
-        filename: 'app.bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPluging()
+  mode: 'development',
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPluging({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
     ]
+  }
 }
