dd6a7e3f3b2de5a28fd10e4f58988f5511de7982
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-11-29 21:50:29 -0400

    lint fix

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,25 +2,25 @@ import React, { useState, useEffect } from 'react'
 import { Category } from './../Category/index'
 import { List, Item } from './styles'
 
-function useCategoriesData() {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(true)
     window.fetch('https://petgram-server-doblesesays.vercel.app/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
-        setLoading(false);
+        setLoading(false)
       })
   }, [])
 
-  return { categories, loading };
+  return { categories, loading }
 }
 
 export const ListOfCategories = () => {
-  const { categories, loading } = useCategoriesData();
+  const { categories, loading } = useCategoriesData()
   const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
@@ -29,7 +29,7 @@ export const ListOfCategories = () => {
       showFixed !== newShowFixed && setShowFixed(newShowFixed)
     }
 
-    document.addEventListener('scroll', onScroll);
+    document.addEventListener('scroll', onScroll)
 
     return () => document.removeEventListener('scroll', onScroll)
   }, [showFixed])
@@ -37,12 +37,11 @@ export const ListOfCategories = () => {
   const renderList = (fixed) => (
     <List fixed={fixed}>
       {
-          loading ? 
-          <Item key="loading"><Category /></Item>
-          :
-          categories.map(category =>
-            <Item key={category.id}><Category {...category} /></Item>
-          )
+          loading
+            ? <Item key='loading'><Category /></Item>
+            : categories.map(category =>
+              <Item key={category.id}><Category {...category} /></Item>
+            )
       }
     </List>
   )
