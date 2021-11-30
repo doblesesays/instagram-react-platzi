f130304dec98aaba965da1f5627b00587aca5861
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-11-29 21:39:24 -0400

    list of categories on scroll.. useeffect to clean events

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     window.fetch('https://petgram-server-doblesesays.vercel.app/categories')
@@ -13,13 +14,31 @@ export const ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-                categories.map(category =>
-                  <Item key={category.id}><Category {...category} /></Item>
-                )
-            }
+          categories.map(category =>
+            <Item key={category.id}><Category {...category} /></Item>
+          )
+      }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
