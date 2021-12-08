4f801f24e48835bb369011544f7caf7bacd8f500
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-12-07 23:00:06 -0400

    Using render props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { ListOfPhotoCards } from './containers/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './containers/photoCardWithQuery'
 
-export const App = () => (
-  <>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+          </>
+      }
+    </>
+  )
+}
