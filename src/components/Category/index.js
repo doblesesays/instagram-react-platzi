c133ab543a7899917d28b97b46ee460686bdf5e9
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-12-13 22:16:43 -0400

    Usando Link para evitar recargar la página

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Anchor, Image } from './styles'
+import { Link, Image } from './styles'
 
 const DEFAULT_IMG = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({ cover = DEFAULT_IMG, path, emoji = '?' }) => (
-  <Anchor href={path}>
+export const Category = ({ cover = DEFAULT_IMG, path = '#', emoji = '?' }) => (
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
