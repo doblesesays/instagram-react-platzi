6bca92c4340d4dff47ec8f8d565131105bc078e5
Genessis Jimenez <genesis.jz.93@gmail.com>
2021-12-15 11:05:22 -0400

    Persistiendo datos en Session Storage

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -1,45 +1,45 @@
-import React from "react"
+import React, { useContext } from "react"
 import { UserForm } from "../components/UserForm"
 import { LoginMutation } from "../containers/LoginMutation";
-import Context from "../Context"
+import { Context } from "../Context"
 import { RegisterMutation } from './../containers/RegisterMutation';
 
-export const NotRegisterUser = () => (
-    <Context.Consumer>
-        {
-            ({ activateAuth }) => {
-                return(
-                    <>
-                        <RegisterMutation>
-                        {
-                            (register, { data, loading, error }) => {
-                                const onSubmit = ({ email, password }) => {
-                                    const input = { email, password }
-                                    const variables = { input }
-                                    register({ variables }).then(activateAuth)
-                                }
-                                const errorMsg = error && 'The user already exist or there is been a problem'
-                                return <UserForm disabled={loading} error={errorMsg} title='Signup' onSubmit={onSubmit} />
-                            }
-                        }
-                        </RegisterMutation>
-                        <LoginMutation>
-                            {
-                                (login, { data, loading, error }) => {
-                                    const onSubmit = ({ email, password }) => {
-                                        const input = { email, password }
-                                        const variables = { input }
-                                        login({ variables }).then(activateAuth)
-                                    }
-                                    const errorMsg = error && 'Incorrect email or password, or there is been another problem'
-                                    return <UserForm disabled={loading} error={errorMsg} title='Login' onSubmit={onSubmit} />
-                                }
-                            }
-                        </LoginMutation>
-                    </>
-                )
-                    
+export const NotRegisterUser = () => {
+    const { activateAuth } = useContext(Context)
+    return(
+        <>
+            <RegisterMutation>
+            {
+                (register, { data, loading, error }) => {
+                    const onSubmit = ({ email, password }) => {
+                        const input = { email, password }
+                        const variables = { input }
+                        register({ variables }).then(({ data }) => {
+                            const { signup } = data
+                            activateAuth(signup)
+                        })
+                    }
+                    const errorMsg = error && 'The user already exist or there is been a problem'
+                    return <UserForm disabled={loading} error={errorMsg} title='Signup' onSubmit={onSubmit} />
+                }
             }
-        }
-    </Context.Consumer>
-)
+            </RegisterMutation>
+            <LoginMutation>
+                {
+                    (login, { data, loading, error }) => {
+                        const onSubmit = ({ email, password }) => {
+                            const input = { email, password }
+                            const variables = { input }
+                            login({ variables }).then(({ data }) => {
+                                const { login } = data
+                                activateAuth(login)
+                            })
+                        }
+                        const errorMsg = error && 'Incorrect email or password, or there is been another problem'
+                        return <UserForm disabled={loading} error={errorMsg} title='Login' onSubmit={onSubmit} />
+                    }
+                }
+            </LoginMutation>
+        </>
+    )
+}
